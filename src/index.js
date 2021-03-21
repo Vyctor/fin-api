a484a6dea3cd81a26bb992775365cee91d4e92c5
Vyctor <dev.vyctor@gmail.com>
2021-03-21 20:46:15 -0300

    cadastro da conta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidv4();
+
+    const user = {
+        id,
+        cpf,
+        name,
+        statement: [],
+    };
+    customers.push(user);
+});
